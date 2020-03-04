3907f99a9eb2d66ce70cd7706b211c8b27b90eca
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-03-04 17:00:23 +0900

    Save Point 1

---
+++ src/api.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const tvPopularParams = {
+  api_key: "e6e0dd53c79220875187320b4265f3d6",
+  language: "en-US"
+};
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/"
+});
+
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing", { params: tvPopularParams }),
+  upComing: () => api.get("movie/upcoming", { params: tvPopularParams }),
+  popular: () => api.get("movie/popular", { params: tvPopularParams }),
+  movieDetail: id =>
+    api.get(`movie/${id}`, {
+      params: {
+        api_key: "e6e0dd53c79220875187320b4265f3d6",
+        language: "en-US",
+        append_to_response: "videos"
+      }
+    }),
+  search: term =>
+    api.get("search/movie", {
+      params: {
+        api_key: "e6e0dd53c79220875187320b4265f3d6",
+        language: "en-US",
+        query: encodeURIComponent(term)
+      }
+    })
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated", { params: tvPopularParams }),
+  popular: () => api.get("tv/popular", { params: tvPopularParams }),
+  airingToday: () => api.get("tv/airing_today", { params: tvPopularParams }),
+  showDetail: id =>
+    api.get(`tv/${id}`, {
+      api_key: "e6e0dd53c79220875187320b4265f3d6",
+      language: "en-US",
+      append_to_response: "videos"
+    }),
+  search: term =>
+    api.get("search/tv", {
+      params: {
+        api_key: "e6e0dd53c79220875187320b4265f3d6",
+        language: "en-US",
+        query: encodeURIComponent(term)
+      }
+    })
+};
